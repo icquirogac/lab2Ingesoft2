5116e2f1a07e1806f6839b447ce20a1e9a99237b
gdguerrerog <gdguerrerog@unal.edu.co>
2019-09-24 18:16:01 -0500

    :ok_hand: new fields on Trip.groovy

--- trip-planner/grails-app/domain/trip/planner/Trip.groovy
+++ trip-planner/grails-app/domain/trip/planner/Trip.groovy
@@ -2,6 +2,14 @@ package trip.planner
 
 class Trip {
 
+    private String name;
+    private String location;
+    private int upVotes;
+    private String hotel;
+    private Date startDate;
+    private Date endDate;
+    private String publisherName;
+
     static constraints = {
     }
 }
