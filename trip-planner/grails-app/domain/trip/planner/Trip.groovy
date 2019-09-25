7625737cdce2fd172c312badcdaef82c13ddf713
icquirogac <icquirogac@unal.edu.co>
2019-09-24 19:32:42 -0500

    :page_facing_up: Updating readMe

--- trip-planner/grails-app/domain/trip/planner/Trip.groovy
+++ trip-planner/grails-app/domain/trip/planner/Trip.groovy
@@ -10,6 +10,15 @@ class Trip {
     private Date endDate;
     private String publisherName;
 
+    public Trip(String nombre, String lugar, String hote, Date startD, Date endD, String publisher){
+    	this.name = nombre;
+    	this.location = lugar;
+    	this.upVotes = 0;
+    	this.hotel = hote;
+    	this.startDate = startD;
+    	this.endDate = endD;
+    	this.publisherName = publisher;
+    }
     static constraints = {
     }
 }
